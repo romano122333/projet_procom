8cd1364b71e843dfea7680139ca31c2358243d57
romano122333 <romain2017l@gmail.com>
2025-02-15 14:54:57 +0100

    Commit 15/02

--- questionnaire.js
+++ questionnaire.js
@@ -1,17 +1,26 @@
+// Données JSON issues des fichiers de cas d'usage
 let jsonData;
+
+// Dictionnaire des modèles et de leurs scores
 let scoresDict = {};
 
+// Indicateur pour savoir si l'algorithme est en cours
 let algo;
 
+// Initialisation du bouton permanent "Relancer"
 const restartButton = document.getElementById('permanent-restart-button');
 restartButton.addEventListener('click', resetAlgorithm);
 
+/**
+ * Affiche la première question : choix du cas d'usage
+ */
 function chooseUseCase() {
     const questionElement = document.getElementById('question');
     const optionsContainer = document.getElementById('options');
 
     questionElement.textContent = 'Quel cas d\'usage souhaitez-vous choisir ?';
 
+    // Mapping des cas d'usage avec leurs fichiers JSON
     const useCases = {
         "Clustering": "json/clustering.json",
         "Classification": "json/classification.json",
@@ -19,8 +28,6 @@ function chooseUseCase() {
         "Prédiction et Régression": "json/prediction.json",
         "Création / Modification de contenu": "json/creation_modification_contenu.json"
     };
-
-    optionsContainer.innerHTML = ''; // Effacer les anciennes options
 
     for (let [useCase, jsonFile] of Object.entries(useCases)) {
         const button = document.createElement('button');
@@ -52,7 +59,9 @@ function loadJSON(jsonFile) {
         });
 }
 
-// Initialiser les scores des algorithmes à 0
+/**
+ * Initialise les scores des algorithmes à zéro
+ */
 function initializeScores(data) {
     scoresDict = {};
     data.questions[0].scores.forEach((scoreObj) => {
@@ -62,14 +71,20 @@ function initializeScores(data) {
     });
 }
 
+/**
+ * Vérifie que la question en cours est viable, c'est-à-dite qu'il n'existe pas une option
+ * qui mette les scores des modèles encore pertinents à jour à -inf.
+ */
 function isViableQuestion(question, aliveModels) {
     const scores = question.scores;
 
-    // Vérifie chaque option de la question
+    // Boucle sur chaque option de la question
     for (let i = 0; i < question.options.length; i++) {
-        let allDead = true;  // Suppose que tous les algorithmes sont à inf_neg pour cette option
+        // Booléen vrai par défaut qui devient faux si il existe un modèle qui n'est pas mis à jour à -inf pour l'option considérée
+        let allDead = true;
 
         for (let alg of aliveModels) {
+            // scoreObj contient les modèles et les scores de la question en cours
             const scoreObj = scores.find(scoreObj => alg in scoreObj);
             const currentScore = scoreObj[alg][i];
             if (currentScore !== "inf_neg") {  // Si au moins un algorithme n'est pas à inf_neg
@@ -78,25 +93,28 @@ function isViableQuestion(question, aliveModels) {
             }
         }
 
-        if (allDead) return false;  // Si une option tue tous les algorithmes vivants, on retourne false
+        if (allDead) return false;
     }
 
-    return true;  // Toutes les options ont au moins un modèle vivant
+    return true;
 }
 
-
-// Fonction pour poser les questions
+/**
+ * Pose les questions aux utilisateurs de manière itérative
+ */
 function askQuestions(idx) {
+    // Vérification que l'arbre n'est pas arrivé à bout pour ne pas mettre à jour les scores avec de nouvelles réponses
     if (algo){
+        // Affichage des scores si on est arrivé en bout d'arbre
         if (idx >= jsonData.questions.length) {
-            showFinalScores(scoresDict); // Afficher les scores finaux
+            showFinalScores(scoresDict);
             return;
         }
 
         const aliveModels = stillAlive();
         const question = jsonData.questions[idx];
 
-
+        // La question est passée si elle n'est pas viable (cf définition de viable dans le README)
         if (!isViableQuestion(question, aliveModels)) {
             askQuestions(idx + 1);
             return;
@@ -105,13 +123,18 @@ function askQuestions(idx) {
         const questionElement = document.getElementById('question');
         const optionsContainer = document.getElementById('options');
 
+        // Modification du contenu de la question
         questionElement.textContent = question.question;
-        optionsContainer.innerHTML = ''; // Effacer les anciennes options
 
+        // Les options sont vidées car toutes les questions n'ont pas le même nombre de réponses
+        optionsContainer.innerHTML = '';
+
+        // Ajout des options
         question.options.forEach((option, i) => {
             const button = document.createElement('button');
             button.textContent = option;
             button.className = 'option-button';
+            // Ajout d'un listener par option
             button.addEventListener('click', () => {
                 handleAnswer(i, question.scores, idx);
                 askQuestions(idx + 1);
@@ -121,7 +144,9 @@ function askQuestions(idx) {
     }
 }
 
-// Retourne la liste des modèles encore disponibles
+/**
+ * Retourne la liste des modèles encore valides (score != -Infinity)
+ */
 function stillAlive() {
     const listeAlive = [];
     for (let alg in scoresDict) {
@@ -133,19 +158,23 @@ function stillAlive() {
     return listeAlive;
 }
 
-// Gérer la réponse de l'utilisateur et mettre à jour les scores
+/**
+ * Gère la réponse de l'utilisateur et met à jour les scores
+ */
 function handleAnswer(selectedOptionIndex, scores, currentQuestionIndex) {
-    if (!algo){
-        return;
-    }
+    // L'algorithme n'est pas executé si on est en bout de branche
+    if (!algo) return;
 
     const question = jsonData.questions[currentQuestionIndex];
     const chosenOption = question.options[selectedOptionIndex];
 
+    // Si aucune analyse de données n'a été effectuée, on arrête l'arbre et on renvoie les scores des modèles
+    // Attention : Pour que cette condition soit prise en compte, il faut que la question soit précisément écrite comme suit.
     if ((question["question"] == "Avez-vous réalisé une analyse des données ?") && (chosenOption == "Non")) {
         showFinalScores(scoresDict);
     }
 
+    // Autrement, on met à jour l'algorithme et les scores
     updateHistory(question.question, chosenOption);
     scores.forEach((scoreObj) => {
         for (let alg in scoreObj) {
@@ -159,19 +188,16 @@ function handleAnswer(selectedOptionIndex, scores, currentQuestionIndex) {
     });
 }
 
-// Afficher le bouton de redémarrage après les résultats
+/**
+ * Affiche les scores finaux
+ */
 function showFinalScores(scoresDict) {
+    // On indique que l'arbre est fini
     algo = false;
-    // Récupère la section des résultats
+
     const resultsSection = document.getElementById('results-section');
     const resultsList = document.getElementById('results-list');
     const restartButton = document.getElementById('restart-button');
-
-    // Vérifie si resultsList existe dans le DOM
-    if (!resultsList) {
-        console.error("L'élément #results-list est introuvable !");
-        return;
-    }
 
     // Vide la liste des résultats pour éviter les doublons
     resultsList.innerHTML = '';
@@ -193,7 +219,11 @@ function showFinalScores(scoresDict) {
     restartButton.addEventListener('click', resetAlgorithm); // Relie le clic au reset
 }
 
+/**
+ * Met à jour l'historique des questions et des réponses choisies
+ */
 function updateHistory(question, chosenOption) {
+    // Vérification que l'arbre n'est pas arrivé à bout pour ne pas mettre à jour les scores avec de nouvelles réponses
     if (algo){
         const historyContainer = document.getElementById('history-container');
 
@@ -213,13 +243,12 @@ function updateHistory(question, chosenOption) {
     }
 }
 
-
-// Fonction pour réinitialiser l'algorithme
+/**
+ * Réinitialise l'algorithme et l'interface utilisateur
+ */
 function resetAlgorithm() {
     // Vider les réponses
-    const historyContainer = document.getElementById('history-container');
-    historyContainer.innerHTML = '';
-
+    document.getElementById('history-container').innerHTML = '';
 
     // Réinitialiser les scores
     for (let key in scoresDict) {
@@ -232,9 +261,9 @@ function resetAlgorithm() {
     const questionElement = document.getElementById('question');
     const restartButton = document.getElementById('restart-button');
 
-    resultsSection.style.display = 'none'; // Cacher les résultats
-    optionsContainer.innerHTML = ''; // Vider les options
-    questionElement.textContent = ''; // Vider la question
+    resultsSection.style.display = 'none';
+    optionsContainer.innerHTML = '';
+    questionElement.textContent = '';
 
     // Réinitialiser l'affichage des scores
     const resultsList = document.getElementById('results-list');
@@ -242,10 +271,12 @@ function resetAlgorithm() {
         resultsList.innerHTML = '';
     }
 
-    restartButton.style.display = 'none'; // Cacher le bouton relancer
+    // Cacher le bouton relancer
+    restartButton.style.display = 'none';
 
     // Redémarrer l'algorithme
     chooseUseCase();
 }
 
+// Démarre le processus avec le choix du cas d'usage
 chooseUseCase();
